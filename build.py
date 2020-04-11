d313e636ff0195a1b13e798e35990075d3130500
alan <alan.vargas0226@gmail.com>
2020-04-10 18:02:01 -0700

    phase3

--- build.py
+++ build.py
@@ -1,3 +1,28 @@
+pages = [ 
+    {
+        "filename": "content/about.html",
+        "output": "docs/about.html",
+        "title": "About Me",
+}, 
+{
+           "filename": "content/contact.html",
+        "output": "docs/contact.html",
+        "title": "contact me",
+}, 
+{
+
+for page in pages:
+    print(pages)
+
+    # Read in the entire template
+    template = open("base.html").read()
+    # Read in the content of the index HTML page
+    index_content = open(page["filename"]).read()
+    # Use the string replace
+    finished_index_page = template.replace("{{content}}", index_content)
+    open("docs/about.html", "w+").write(finished_index_page)
+
+
 def main():
  
 
@@ -20,7 +45,26 @@ def main():
     combined_html = top_html +  middle_html + botton_html
     print(combined_html)
 
+     def print(content):
+     TODO: Read in template, do string replacing, and return result return results
+     def main():
+        content = open(’docs/index.html’).read()
+        resulting_html_for_index = apply_template(content)
+
+        def print(templates):
+     TODO: Read in template, do string replacing, and return result return results
+     def main():
+        content = open(’docs/bottom.html’).read()
+        resulting_html_for_index = apply_template(content)
+
+        def print(top.html):
+     TODO: Read in template, do string replacing, and return result return results
+     def main():
+        content = open('docs/top.html').read()
+        resulting_html_for_index = apply_template(content)
+
+
     #Reads in the middle index.html
 
-    middle_html = open('./content/index.html').read()
+    open('./docs/index.html').write(combined_html)
 main()
